814c5e78fd95c0c499d7e91826e1bc7b953f9073
niscontractor <niscontractor@gmail.com>
2016-12-02 18:26:32 +0530

    changed msg

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/test', function(req, res, next) {
-  res.send('Hello user');
+  res.send('Hello user, this is Jenkins demo');
 });
 
 module.exports = router;
