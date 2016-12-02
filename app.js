c72fa77ac912130163538d59935c507668ffe037
niscontractor <niscontractor@gmail.com>
2016-12-02 17:42:27 +0530

    cors added

--- app.js
+++ app.js
@@ -20,6 +20,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept-Language, Authorization");
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    if ('OPTIONS' === req.method) {
+      res.sendStatus(200);
+    }
+    else 
+      next();
+});
+
 app.use('/api', routes);
 
 // catch 404 and forward to error handler
